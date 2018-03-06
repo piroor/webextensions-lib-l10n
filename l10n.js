a974222d2f54da5f48b856557bd4118925ae7e60
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2018-03-07 00:55:21 +0900

    Modernize coding style

--- l10n.js
+++ l10n.js
@@ -5,52 +5,49 @@
 */
 
 var l10n = {
-	updateString(aString) {
-		return aString.replace(/__MSG_(.+?)__/g, function(aMatched) {
-			var key = aMatched.slice(6, -2);
-			return chrome.i18n.getMessage(key) || aMatched;
-		});
-	},
+  updateString(aString) {
+    return aString.replace(/__MSG_(.+?)__/g, function(aMatched) {
+      const key = aMatched.slice(6, -2);
+      return chrome.i18n.getMessage(key) || aMatched;
+    });
+  },
 
-	$log(aMessage, ...aArgs) {
-		aMessage = 'l10s ' + aMessage;
-		if (typeof log === 'function')
-			log(aMessage, ...aArgs);
-		else
-			console.log(aMessage, ...aArgs);
-	},
+  $log(aMessage, ...aArgs) {
+    aMessage = `l10s: ${aMessage}`;
+    if (typeof window.log === 'function')
+      log(aMessage, ...aArgs);
+    else
+      console.log(aMessage, ...aArgs);
+  },
 
-	updateDocument() {
-		var texts = document.evaluate(
-				'descendant::text()[contains(self::text(), "__MSG_")]',
-				document,
-				null,
-				XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
-				null
-			);
-		for (let i = 0, maxi = texts.snapshotLength; i < maxi; i++)
-		{
-			let text = texts.snapshotItem(i);
-			text.nodeValue = this.updateString(text.nodeValue);
-		}
+  updateDocument() {
+    const texts = document.evaluate(
+      'descendant::text()[contains(self::text(), "__MSG_")]',
+      document,
+      null,
+      XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+      null
+    );
+    for (let i = 0, maxi = texts.snapshotLength; i < maxi; i++) {
+      const text = texts.snapshotItem(i);
+      text.nodeValue = this.updateString(text.nodeValue);
+    }
 
-		var attributes = document.evaluate(
-				'descendant::*/attribute::*[contains(., "__MSG_")]',
-				document,
-				null,
-				XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
-				null
-			);
-		for (let i = 0, maxi = attributes.snapshotLength; i < maxi; i++)
-		{
-			let attribute = attributes.snapshotItem(i);
-			this.$log('apply', attribute);
-			attribute.value = this.updateString(attribute.value);
-		}
-	}
+    const attributes = document.evaluate(
+      'descendant::*/attribute::*[contains(., "__MSG_")]',
+      document,
+      null,
+      XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+      null
+    );
+    for (let i = 0, maxi = attributes.snapshotLength; i < maxi; i++) {
+      const attribute = attributes.snapshotItem(i);
+      this.$log('apply', attribute);
+      attribute.value = this.updateString(attribute.value);
+    }
+  }
 };
 
-document.addEventListener('DOMContentLoaded', function onReady() {
-	document.removeEventListener('DOMContentLoaded', onReady);
-	l10n.updateDocument();
-});
+document.addEventListener('DOMContentLoaded', () => {
+  l10n.updateDocument();
+}, { once: true });
